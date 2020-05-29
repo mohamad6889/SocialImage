7eb07549331b6a7ee2e511cf65d422ca4c330f27
Mohammad Amani <mohamad.amani68@gmail.com>
2020-05-29 12:39:18 +0430

    first

---
+++ src/main/java/com/social/image/socialimage/SocialimageApplication.java
@@ -0,0 +1,13 @@
+package com.social.image.socialimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialimageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialimageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/social/image/socialimage/SocialimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.image.socialimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialimageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
